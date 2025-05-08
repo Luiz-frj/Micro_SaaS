ddd0ad0d89a53e8dce1432834bf76db4aeb6f9ab
guilhermefragiacomo <guilhermefragiacomo1@gmail.com>
2025-05-08 17:13:07 -0300

    update BD

--- BD.sql
+++ BD.sql
@@ -4,14 +4,14 @@ USE SaaS;
 
 CREATE TABLE Portifolio(
 	id_img INT PRIMARY KEY AUTO_INCREMENT,
-    id_prestador INT NOT NULL,
+    id_servico INT NOT NULL,
     caminho_img VARCHAR(255),
     
     FOREIGN KEY (id_prestador) REFERENCES Prestador(id_prestador)
 );
 
 CREATE TABLE Cliente(
-	id_contratante INT AUTO_INCREMENT PRIMARY KEY,
+	id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(50) NOT NULL, -- criptografar
     nome VARCHAR(100) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE Prestador(
     nome_fantasia VARCHAR(100) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
     cpf VARCHAR(100) NOT NULL UNIQUE,
-    foto VARCHAR(255)
+    caminho_img VARCHAR(255)
 );
 
 CREATE TABLE Servico(
@@ -36,16 +36,16 @@ CREATE TABLE Servico(
     preco DOUBLE NOT NULL,
     descricao VARCHAR(100),
     id_diponibilidade INT NOT NULL,
-    horario DATETIME,
-    status_horario ENUM('Disponivel', 'Indisponivel') DEFAULT 'Disponivel',
-    status_servico ENUM('Aceito', 'Recusado') DEFAULT 'Aceito',
-    
-    FOREIGN KEY (id_prestador) REFERENCES Prestador(id_prestador)
+    status_servico ENUM('Disponivel', 'Indisponivel') DEFAULT 'Disponivel',
+    local VARCHAR(100),
+
+    FOREIGN KEY (id_prestador) REFERENCES Prestador(id_prestador),
+    FOREIGN KEY (id_disponibilidade) REFERENCES Disponibilidade(id_disponibilidade)
 );
 
 CREATE TABLE Disponibilidade(
 	id_disponilidade INT PRIMARY KEY AUTO_INCREMENT,
-    id_prestador INT NOT NULL,
+    id_servico INT NOT NULL,
     dia_semana INT NOT NULL,
     comeco_descanso TIME NOT NULL,
     fim_descanso TIME NOT NULL,
@@ -53,8 +53,18 @@ CREATE TABLE Disponibilidade(
     fim_servico TIME NOT NULL,
     tempo_servico TIME NOT NULL,
     
-    FOREIGN KEY (id_prestador) REFERENCES Prestador(id_prestador),
-    FOREIGN KEY (id_disponibilidade) REFERENCES Servico(id_diponibilidade),
-    UNIQUE(id_prestador, dia_semana, comeco_descanso, fim_descanso, inicio_servico, fim_servico, tempo_servico)
+    FOREIGN KEY (id_servico) REFERENCES Servico(id_servico),
+    UNIQUE(id_servico, dia_semana, comeco_descanso, fim_descanso, inicio_servico, fim_servico, tempo_servico)
+);
+
+CREATE TABLE Agendamento(
+	id_agendamento INT PRIMARY KEY AUTO_INCREMENT,
+    id_servico INT NOT NULL,
+    id_cliente INT NOT NULL,
+    status_agendamento ENUM('Ativo', 'Finalizado', 'Cancelado') DEFAULT 'Ativo',
+    horario DATETIME,
+    
+    FOREIGN KEY (id_servico) REFERENCES Servico(id_servico),
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
 );
 
